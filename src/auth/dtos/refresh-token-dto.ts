bf8ab60b6a991003a616294d5b4668a7bebd2a43
supalonely <danilbashirov0@mail.ru>
2025-04-15 03:57:18 +0500

    refresh token fixed

--- src/auth/dtos/refresh-token-dto.ts
+++ src/auth/dtos/refresh-token-dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import { IsNotEmpty } from "class-validator";
 
 export class RefreshTokenDto {
   @ApiProperty({ title: 'Refresh token', example: 'refresh_token' })
+  @IsString()
+  @IsNotEmpty()
   refreshToken: string;
 }
